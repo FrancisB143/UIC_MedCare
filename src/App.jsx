30ae0e5307aed4fbbdfed85cb295323abfe24b4f
FrancisB143 <fbangoy_230000001354@uic.edu.ph>
2025-07-10 22:49:43 +0800

    Formatting Code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <body class="bg-pink-500">
+        
+      </body>
+    </>
+  )
+}
+
+export default App
