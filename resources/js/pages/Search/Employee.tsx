4559d727d825f04ff93789cb0b47ec77a4c3873f
FrancisB143 <fbangoy_230000001354@uic.edu.ph>
2025-09-20 22:42:51 +0800

    print.tsx
    -added the content of print.tsx

--- resources/js/pages/Search/Employee.tsx
+++ resources/js/pages/Search/Employee.tsx
@@ -86,7 +86,7 @@ const Employee: React.FC = () => {
                             <input
                                 type="text"
                                 placeholder="Search employees..."
-                                className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-[#A3386C] focus:border-[#A3386C] outline-none"
+                                className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-[#A3386C] focus:border-[#A3386C] outline-none text-black"
                                 value={searchTerm}
                                 onChange={(e) => setSearchTerm(e.target.value)}
                             />
